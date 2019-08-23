79a8783762a85b400a9c98243b68ed28606a3a10
Viacheslav_Plekhanov <viacheslav_plekhanov@epam.com>
2019-08-23 23:09:13 +0300

    add SQLite db

--- src/main/java/com/revolut/accountservice/App.java
+++ src/main/java/com/revolut/accountservice/App.java
@@ -1,14 +1,30 @@
-package com.revolut.accountservice;/*
- * Copyright 2001-2019 by HireRight, Inc. All rights reserved.
- * This software is the confidential and proprietary information
- * of HireRight, Inc. Use is subject to license terms.
- */
+package com.revolut.accountservice;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static spark.Spark.get;
 
 public class App
 {
 	public static void main(String[] args) {
-		get("/hello", (req, res) -> "Hello, World!");
+//		get("/hello", (req, res) -> "Hello, World!");
+		createNewDatabase("DB");
+		
+	}
+	
+	public static void createNewDatabase(String name){
+		String url = "jdbc:sqlite:memory:" + name;
+		try(Connection connection = DriverManager.getConnection(url))
+		{
+			DatabaseMetaData meta = connection.getMetaData();
+			System.out.println("The driver name is " + meta.getDriverName());
+			System.out.println("A new database has been created.");
+			
+		} catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
